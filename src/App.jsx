449e96b47e6587bb198383aab0ad64ba0716b76c
Domantas <domantasdigital@gmail.com>
2026-05-21 15:04:40 +0300

    add button style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
 const App = () => {
-  return (
-    <div className=" text-grey-800">
-      <h1>
-        <i>lalala</i>
-      </h1>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
