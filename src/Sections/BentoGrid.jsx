b0800080fb558e50c180a475123ab9154144b0ca
Domantas <domantasdigital@gmail.com>
2026-05-25 12:32:30 +0300

    working just with a slight jump sometimes

--- src/Sections/BentoGrid.jsx
+++ src/Sections/BentoGrid.jsx
@@ -9,32 +9,35 @@ gsap.registerPlugin(Flip);
 const BentoGrid = () => {
   const [expanded, setExpanded] = useState(null);
   const containerRef = useRef(null);
+  const isTransitioning = useRef(false);
 
   const handleMouseEnter = (e) => {
-    // if (expanded) return; // don't hover-animate when something is expanded
-    // gsap.to(e.currentTarget, {
-    //   scale: 1.03,
-    //   boxShadow: "0 0 0 3px #ED5A11",
-    //   duration: 0.3,
-    //   ease: "power2.out",
-    // });
+    if (expanded) return; // don't hover-animate when something is expanded
+    gsap.to(e.currentTarget, {
+      scale: 1.02,
+      boxShadow: "0 0 0 3px #ED5A11",
+      duration: 0.3,
+      ease: "power2.out",
+    });
   };
 
   const handleMouseLeave = (e) => {
-    // if (expanded) return;
-    // gsap.to(e.currentTarget, {
-    //   scale: 1,
-    //   boxShadow: "0 0 0 0px #ED5A11",
-    //   duration: 0.3,
-    //   ease: "power2.out",
-    // });
+    if (expanded) return;
+    gsap.to(e.currentTarget, {
+      scale: 1,
+      boxShadow: "0 0 0 0px #ED5A11",
+      duration: 0.3,
+      ease: "power2.out",
+    });
   };
 
   const handleExpand = (id) => {
     // const box = containerRef.current.querySelector(`.${id}`);
-    // gsap.set(box, {
+
+    // gsap.to(box, {
     //   scale: 1,
-    //   boxShadow: "0 0 0 0px #ED5A11",
+    //   boxShadow: "0 0 0 3px #ED5A11",
+    //   duration: 5,
     // });
 
     const state = Flip.getState(
@@ -43,21 +46,19 @@ const BentoGrid = () => {
     flushSync(() => setExpanded(id));
 
     Flip.from(state, {
-      duration: 0.5,
+      duration: 1,
+      absolute: true,
       ease: "power2.inOut",
-      onStart: () => {
-        gsap.to(containerRef.current.querySelectorAll(".boxes.collapsed"), {
-          opacity: 0,
-          scale: 0.9,
-          duration: 0.3,
-          ease: "power2.out",
-        });
-      },
+      //   scale: 1,
+      boxShadow: "0 0 0 0px #FAF8F7",
+      clearProps: "scale,boxShadow",
+
       onLeave: (elements) =>
         gsap.to(elements, {
           opacity: 0,
           scale: 0,
-          duration: 1,
+          duration: 0.3,
+          stagger: 0.05,
         }),
     });
   };
@@ -72,14 +73,14 @@ const BentoGrid = () => {
     flushSync(() => setExpanded(null));
 
     Flip.from(state, {
-      duration: 0.5,
+      duration: 0.8,
       ease: "power2.inOut",
 
       onEnter: (elements) =>
         gsap.fromTo(
           elements,
           { opacity: 0 },
-          { opacity: 1, delay: 0.3, duration: 1, stagger: 0.1 },
+          { opacity: 1, delay: 0.8, duration: 1.2, stagger: 0.1 },
         ),
     });
   };
@@ -113,45 +114,75 @@ const BentoGrid = () => {
           style={{ gridArea: "box-2" }}
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
+          onClick={() => !expanded && handleExpand("box-2")}
         >
           <h4>Box 2</h4>
           <p>This text should only be seen when Box 2 was clicked to expand</p>
+          {expanded === "box-2" && (
+            <button className="close-btn" onClick={handleClose}>
+              ✕
+            </button>
+          )}
         </div>
         <div
           className={`${boxClass("box-3")} bg-red-500`}
           style={{ gridArea: "box-3" }}
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
+          onClick={() => !expanded && handleExpand("box-3")}
         >
           <h4>Box 3</h4>
           <p>This text should only be seen when Box 3 was clicked to expand</p>
+          {expanded === "box-3" && (
+            <button className="close-btn" onClick={handleClose}>
+              ✕
+            </button>
+          )}
         </div>
         <div
           className={`${boxClass("box-4")} bg-blue-300`}
           style={{ gridArea: "box-4" }}
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
+          onClick={() => !expanded && handleExpand("box-4")}
         >
           <h4>Box 4</h4>
           <p>This text should only be seen when Box 4 was clicked to expand</p>
+          {expanded === "box-4" && (
+            <button className="close-btn" onClick={handleClose}>
+              ✕
+            </button>
+          )}
         </div>
         <div
           className={`${boxClass("box-5")} bg-orange-300`}
           style={{ gridArea: "box-5" }}
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
+          onClick={() => !expanded && handleExpand("box-5")}
         >
           <h4>Box 5</h4>
           <p>This text should only be seen when Box 5 was clicked to expand</p>
+          {expanded === "box-5" && (
+            <button className="close-btn" onClick={handleClose}>
+              ✕
+            </button>
+          )}
         </div>
         <div
           className={`${boxClass("box-6")} bg-purple-400`}
           style={{ gridArea: "box-6" }}
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
+          onClick={() => !expanded && handleExpand("box-6")}
         >
           <h4>Box 6</h4>
           <p>This text should only be seen when Box 6 was clicked to expand</p>
+          {expanded === "box-6" && (
+            <button className="close-btn" onClick={handleClose}>
+              ✕
+            </button>
+          )}
         </div>
       </div>
     </div>
