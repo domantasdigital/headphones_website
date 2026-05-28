bc31213862177d4b889df4cabcbfd40917334670
Domantas <domantasdigital@gmail.com>
2026-05-28 09:39:03 +0300

    add full bento grid section to mobile and desktop

---
+++ src/Components/BentoGrid_Section_Content.js
@@ -0,0 +1,38 @@
+const bContent = {
+  card1: {
+    description:
+      "Experience your music untethered.With Qualcomm® aptX™ HD andAAC codecs, the Amiron Wireless delivers high-resolution Bluetooth® audio that rivals any wired connection",
+
+    break: "Bluetooth® 4.2 / Range up to 10 meters / 30+ hour battery life.",
+  },
+  card2: {
+    description:
+      "Our hearing is as unique as a fingerprint, and it changes over our lifetime. Through a quick hearing test powered by Mimi Defined™ in the MIY app, the Amiron Wireless dynamically adjusts its sound profile to perfectly compensate for your individual hearing. Missing details are restored, bringing your favorite tracks back to full brilliance.",
+  },
+  card3: {
+    description:
+      "Every single Amiron Wireless is handcrafted by experts in Germany. By utilizing premium materials like Alcantara® and high-grade aluminum alongside our legendary Tesla transducer technology, we ensure a flawless acoustic experience and rock-solid durability. ",
+
+    break:
+      "Featuring Tesla technology for powerful, distortion-free sound reproduction.",
+  },
+  card4: {
+    description:
+      "The entire right ear cup acts as a highly responsive touchpad. Smoothly adjust the volume, skip tracks, pause your music, or summon your smartphone’s voice assistant without ever reaching into your pocket.",
+    ul: {
+      liFirst: "Swipe Up/Down: Volume Control",
+      liSecond: "Right: Next/Previous",
+      liThird: "Play/Pause/Call Answer",
+    },
+  },
+
+  card5: {
+    description:
+      "Designed for long, immersive listening sessions. The balanced headband pressure ensures a secure yet incredibly soft fit. The ear cushions are wrapped in premium, breathable Alcantara® microfiber, which actively dissipates heat and moisture for ultimate comfort. ",
+
+    break:
+      "Lightweight design / Circumaural (over-ear) fit /Replaceable ear pads.",
+  },
+};
+
+export default bContent;
--- src/Sections/BentoGrid.jsx
+++ src/Sections/BentoGrid.jsx
@@ -3,19 +3,36 @@ import { gsap } from "gsap";
 import { Flip } from "gsap/Flip";
 import { flushSync } from "react-dom";
 import "../CSS for specific sections/BentoGridCss.css";
+import "../CSS for specific sections/BentoPhone.css";
+import { SplitText } from "gsap/SplitText";
+
+import card1 from "/Assets/Card_1.png";
+import card2 from "/Assets/Card_2.png";
+import card3 from "/Assets/Card_3.png";
+import card4 from "/Assets/Card_4.png";
+import card5 from "/Assets/Card_5.png";
+
+import bContent from "../Components/BentoGrid_Section_Content.js";
 
 gsap.registerPlugin(Flip);
+gsap.registerPlugin(SplitText);
 
 const BentoGrid = () => {
   const [expanded, setExpanded] = useState(null);
   const containerRef = useRef(null);
-  //   const isTransitioning = useRef(false);
+
+  const box1DescriptionRef = useRef(null);
+  const box2DescriptionRef = useRef(null);
+  const box3DescriptionRef = useRef(null);
+  const box4DescriptionRef = useRef(null);
+  const box4ULRef = useRef(null);
+  const box5DescriptionRef = useRef(null);
 
   const handleMouseEnter = (e) => {
     if (expanded) return; // don't hover-animate when something is expanded
     gsap.to(e.currentTarget, {
       scale: 1.02,
-      boxShadow: "0 0 0 3px #ED5A11",
+      boxShadow: "0 0 0 2px #ED5A11",
       duration: 0.2,
       ease: "power2.out",
     });
@@ -62,6 +79,101 @@ const BentoGrid = () => {
           stagger: 0.05,
         }),
     });
+
+    // ANIMATING DESCRIPTIONS-------------------------------
+
+    const split1 = new SplitText(box1DescriptionRef.current, {
+      type: "lines",
+    });
+    gsap.fromTo(
+      split1.lines,
+      { opacity: 0, y: 20 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 0.6,
+        delay: 1,
+        stagger: 0.15,
+        ease: "power2.out",
+      },
+    );
+
+    const split2 = new SplitText(box2DescriptionRef.current, {
+      type: "lines",
+    });
+    gsap.fromTo(
+      split2.lines,
+      { opacity: 0, y: 20 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 0.6,
+        delay: 1,
+        stagger: 0.15,
+        ease: "power2.out",
+      },
+    );
+
+    const split3 = new SplitText(box3DescriptionRef.current, {
+      type: "lines",
+    });
+    gsap.fromTo(
+      split3.lines,
+      { opacity: 0, y: 20 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 0.6,
+        delay: 1,
+        stagger: 0.15,
+        ease: "power2.out",
+      },
+    );
+
+    const split4 = new SplitText(box4DescriptionRef.current, {
+      type: "lines",
+    });
+    gsap.fromTo(
+      split4.lines,
+      { opacity: 0, y: 20 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 0.6,
+        delay: 1,
+        stagger: 0.15,
+        ease: "power2.out",
+      },
+    );
+    const split4ULItems = box4ULRef.current.querySelectorAll("li");
+    gsap.fromTo(
+      split4ULItems,
+      { opacity: 0, y: 20 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 0.6,
+        delay: 1.8,
+        stagger: 0.15,
+        ease: "power2.out",
+      },
+    );
+
+    const split5 = new SplitText(box5DescriptionRef.current, {
+      type: "lines",
+    });
+    gsap.fromTo(
+      split5.lines,
+      { opacity: 0, y: 20 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 0.6,
+        delay: 1,
+        stagger: 0.15,
+        ease: "power2.out",
+      },
+    );
   };
 
   const handleClose = (e) => {
@@ -84,26 +196,189 @@ const BentoGrid = () => {
           { opacity: 1, delay: 0.8, duration: 1.2, stagger: 0.1 },
         ),
     });
+
+    // Handling phone version
+  };
+  const [phoneGridOpen, setPhoneGridOpen] = useState(false);
+
+  const toggle = (e, id) => {
+    const el = e.currentTarget.querySelector("[data-content]");
+
+    // close previously open card if different one is clicked
+    if (phoneGridOpen && phoneGridOpen !== id) {
+      const prevEl = document.querySelector(
+        `[data-id="${phoneGridOpen}"] [data-content]`,
+      );
+      if (prevEl)
+        gsap.to(prevEl, {
+          height: 0,
+          opacity: 0,
+          duration: 0.3,
+          ease: "power2.in",
+        });
+    }
+
+    if (phoneGridOpen !== id) {
+      gsap.fromTo(
+        el,
+        { height: 0, opacity: 0 },
+        { height: "auto", opacity: 1, duration: 0.4, ease: "power2.out" },
+      );
+      setPhoneGridOpen(id);
+    } else {
+      gsap.to(el, { height: 0, opacity: 0, duration: 0.3, ease: "power2.in" });
+      setPhoneGridOpen(null);
+    }
   };
 
   const boxClass = (id) =>
     `boxes ${id} ${expanded === id ? "expanded" : ""} ${expanded && expanded !== id ? "collapsed" : ""}`;
 
   return (
-    <div className="h-screen mt-50 px-20 ">
+    <div className="md:h-screen mt-15 md:mt-15 max-w-7xl w-full mx-auto px-2 ">
+      {/* The vertical grid layout for phones */}
+
       <div
-        className={`grid-container ${expanded ? "has-expanded" : ""}`}
+        onClick={(e) => toggle(e, "card1")}
+        data-id="card1"
+        className="rounded-2xl bg-[#f5f0eb] overflow-hidden mb-4 cursor-pointer pBox-1 block md:hidden "
+      >
+        {/* image + label row — always visible */}
+        <div className="flex flex-center justify-between  px-7 py-4 h-[15vh] ">
+          <img src={card1} className="w-24 h-24 object-contain pBox-1-img" />
+          <h6 className="body-comment">Listen freely.</h6>
+        </div>
+
+        {/* expandable content */}
+        <div data-content style={{ height: 0, overflow: "hidden" }}>
+          <p className="px-6 pb-6 text-sm text-gray-600">
+            {bContent.card1.description}
+          </p>
+          <p className="px-6 pb-6 text-sm text-gray-600">
+            <i>{bContent.card1.break}</i>
+          </p>
+        </div>
+      </div>
+
+      <div
+        onClick={(e) => toggle(e, "card2")}
+        data-id="card2"
+        className="rounded-2xl mb-4 bg-[#f5f0eb] overflow-hidden cursor-pointer pBox-2 block md:hidden "
+      >
+        {/* image + label row — always visible */}
+        <div className="flex flex-center justify-between  px-7 py-4 h-[15vh] ">
+          <h6 className="body-comment">Listen freely.</h6>
+          <img src={card2} className="w-24 h-24 object-contain pBox-2-img" />
+        </div>
+
+        {/* expandable content */}
+        <div data-content style={{ height: 0, overflow: "hidden" }}>
+          <p className="px-6 pb-6 text-sm text-gray-600">
+            {bContent.card2.description}
+          </p>
+        </div>
+      </div>
+      <div
+        onClick={(e) => toggle(e, "card3")}
+        data-id="card3"
+        className="rounded-2xl mb-4 bg-[#f5f0eb] overflow-hidden cursor-pointer pBox-3 block md:hidden "
+      >
+        {/* image + label row — always visible */}
+        <div className="flex flex-center justify-between  px-7 py-4 h-[15vh] ">
+          <img src={card3} className="w-24 h-24 object-contain pBox-3-img" />
+          <h6 className="body-comment">Craftsmanship.</h6>
+        </div>
+
+        {/* expandable content */}
+        <div data-content style={{ height: 0, overflow: "hidden" }}>
+          <p className="px-6 pb-6 text-sm text-gray-600">
+            {bContent.card3.description}
+          </p>
+          <p className="px-6 pb-6 text-sm text-gray-600">
+            <i>{bContent.card3.break}</i>
+          </p>
+        </div>
+      </div>
+      <div
+        onClick={(e) => toggle(e, "card4")}
+        data-id="card4"
+        className="rounded-2xl mb-4 bg-[#f5f0eb] overflow-hidden cursor-pointer pBox-4 block md:hidden "
+      >
+        {/* image + label row — always visible */}
+        <div className="flex flex-center justify-between  px-7 py-4 h-[15vh] ">
+          <h6 className="body-comment">Control & interact.</h6>
+          <img src={card4} className="w-24 h-24 object-contain pBox-4-img" />
+        </div>
+
+        {/* expandable content */}
+        <div data-content style={{ height: 0, overflow: "hidden" }}>
+          <p className="px-6 pb-6 text-sm text-gray-600">
+            {bContent.card4.description}
+          </p>
+          <ul className="px-6 pb-6 text-sm text-gray-600">
+            <i>
+              <li>{bContent.card4.ul.liFirst}</li>
+              <li>{bContent.card4.ul.liSecond}</li>
+              <li>{bContent.card4.ul.liThird}</li>
+            </i>
+          </ul>
+        </div>
+      </div>
+      <div
+        onClick={(e) => toggle(e, "card5")}
+        data-id="card5"
+        className="rounded-2xl mb-4 bg-[#f5f0eb] overflow-hidden cursor-pointer pBox-5 block md:hidden "
+      >
+        {/* image + label row — always visible */}
+        <div className="flex flex-center justify-between  px-7 py-4 h-[15vh] ">
+          <img src={card5} className="w-24 h-24 object-contain pBox-5-img" />
+          <h6 className="body-comment">Comfort.</h6>
+        </div>
+
+        {/* expandable content */}
+        <div data-content style={{ height: 0, overflow: "hidden" }}>
+          <p className="mt-9 px-6 pb-6 text-sm text-gray-600">
+            {bContent.card5.description}
+          </p>
+          <p className="px-6 pb-6 text-sm text-gray-600">
+            <i>{bContent.card5.break}</i>
+          </p>
+        </div>
+      </div>
+
+      {/* The Bento grid layout for tablets and desktop 
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      */}
+      <div
+        className={`grid-container ${expanded ? "has-expanded" : ""} mx-auto display-none md:flex`}
         ref={containerRef}
       >
         <div
-          className={`${boxClass("box-1")} bg-green-400`}
+          className={`${boxClass("box-1")}`}
           style={{ gridArea: "box-1" }}
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
           onClick={() => !expanded && handleExpand("box-1")}
         >
-          <h4>Box 1</h4>
-          <p>This text should only be seen when Box 1 was clicked to expand</p>
+          <img src={card1} alt="" className="card-image" />
+          <h6 className="card-label">Listen freely.</h6>
+          <p className="description" ref={box1DescriptionRef}>
+            {bContent.card1.description}
+            <br />
+            <br />
+            <i>{bContent.card1.break}</i>
+          </p>
+
           {expanded === "box-1" && (
             <button className="close-btn" onClick={handleClose}>
               ✕
@@ -117,8 +392,11 @@ const BentoGrid = () => {
           onMouseLeave={handleMouseLeave}
           onClick={() => !expanded && handleExpand("box-2")}
         >
-          <h4>Box 2</h4>
-          <p>This text should only be seen when Box 2 was clicked to expand</p>
+          <img src={card2} alt="" className="card-image" />
+          <h6 className="card-label">Personalize for you.</h6>
+          <p className="description" ref={box2DescriptionRef}>
+            {bContent.card2.description}
+          </p>
           {expanded === "box-2" && (
             <button className="close-btn" onClick={handleClose}>
               ✕
@@ -132,8 +410,14 @@ const BentoGrid = () => {
           onMouseLeave={handleMouseLeave}
           onClick={() => !expanded && handleExpand("box-3")}
         >
-          <h4>Box 3</h4>
-          <p>This text should only be seen when Box 3 was clicked to expand</p>
+          <img src={card3} alt="" className="card-image" />
+          <h6 className="card-label">Craftsmanship.</h6>
+          <p className="description" ref={box3DescriptionRef}>
+            {bContent.card3.description}
+            <br />
+            <br />
+            <i>{bContent.card3.break}</i>
+          </p>
           {expanded === "box-3" && (
             <button className="close-btn" onClick={handleClose}>
               ✕
@@ -147,8 +431,21 @@ const BentoGrid = () => {
           onMouseLeave={handleMouseLeave}
           onClick={() => !expanded && handleExpand("box-4")}
         >
-          <h4>Box 4</h4>
-          <p>This text should only be seen when Box 4 was clicked to expand</p>
+          <img src={card4} alt="" className="card-image" />
+          <h6 className="card-label">Control & interact.</h6>
+          <p className="description" ref={box4DescriptionRef}>
+            {bContent.card4.description}
+            <br />
+            <br />
+          </p>
+          <ul className="unorderedList" ref={box4ULRef}>
+            <i>
+              <li>{bContent.card4.ul.liFirst}</li>
+              <li>{bContent.card4.ul.liSecond}</li>
+              <li>{bContent.card4.ul.liThird}</li>
+            </i>
+          </ul>
+
           {expanded === "box-4" && (
             <button className="close-btn" onClick={handleClose}>
               ✕
@@ -162,24 +459,15 @@ const BentoGrid = () => {
           onMouseLeave={handleMouseLeave}
           onClick={() => !expanded && handleExpand("box-5")}
         >
-          <h4>Box 5</h4>
-          <p>This text should only be seen when Box 5 was clicked to expand</p>
+          <img src={card5} alt="" className="card-image" />
+          <h6 className="card-label">Comfort.</h6>
+          <p className="description" ref={box5DescriptionRef}>
+            {bContent.card5.description}
+            <br />
+            <br />
+            <i>{bContent.card5.break}</i>
+          </p>
           {expanded === "box-5" && (
-            <button className="close-btn" onClick={handleClose}>
-              ✕
-            </button>
-          )}
-        </div>
-        <div
-          className={`${boxClass("box-6")} bg-purple-400`}
-          style={{ gridArea: "box-6" }}
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-          onClick={() => !expanded && handleExpand("box-6")}
-        >
-          <h4>Box 6</h4>
-          <p>This text should only be seen when Box 6 was clicked to expand</p>
-          {expanded === "box-6" && (
             <button className="close-btn" onClick={handleClose}>
               ✕
             </button>
